a3cd8d3b1b8880768bb3abae5c48f771a4e29e0b
hossein <hosseingarosian@gmail.com>
2025-09-23 19:30:34 +0330

    created function for pressing buttons

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from tkinter import *
 
 
 
+#functions and variabes
+
+equation_text = ""
+
+
+def button_pressing(num):
+
+    global equation_text
+
+    equation_text = equation_text + str(num)
+
+    equation_label.set(equation_text)
+
+
+
+
+
+
 # window config
 
 window = Tk()
