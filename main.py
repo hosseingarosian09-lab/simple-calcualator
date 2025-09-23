23ca8e37f2a01d0e2a0ad8c8576bad458a7a0a06
hossein <hosseingarosian@gmail.com>
2025-09-23 20:01:39 +0330

    crated equals and clear function

--- main.py
+++ main.py
@@ -10,12 +10,41 @@ equation_text = ""
 def button_pressing(num):
 
     global equation_text
-
     equation_text = equation_text + str(num)
-
     equation_label.set(equation_text)
 
 
+def clear ():
+
+    global equation_text
+    equation_label.set("")
+    equation_text = ""
+
+
+def equals ():
+    global equation_text
+
+    try:
+
+
+        total = str(eval(equation_text))
+
+        equation_label.set(total)
+
+        equation_text = total
+
+  
+
+    except SyntaxError:
+
+        equation_label.set("syntax error")
+        equation_text = ""
+
+  
+    except ZeroDivisionError:
+
+        equation_label.set("arithmetic error")
+        equation_text = ""
 
 
 
