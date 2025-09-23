7d80b4731e9a8eb8cd50571e44a75e42e9b305bc
hossein <hosseingarosian@gmail.com>
2025-09-23 19:11:30 +0330

    creating window

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+# window config
+
+  
+
+window = Tk()
+
+window.title("calculator")
+
+window.geometry("400x400")
+
+window.config(bg="black")
+
+
+
+
+
+window.mainloop()
