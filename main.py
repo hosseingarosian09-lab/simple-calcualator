5b122b31ba2f4b8347d60e0daa89b8004988928c
hossein <hosseingarosian@gmail.com>
2025-09-24 14:22:50 +0330

    framing and resizing

--- main.py
+++ main.py
@@ -54,92 +54,95 @@ def equals ():
 window = Tk()
 
 window.title("calculator")
-window.config(bg="black")
+window.geometry("350x400")
+window.config(bd=5, relief="groove")
 
 
 #label
 equation_label = StringVar()
 label = Label(window, textvariable=equation_label, bg="gray", height=2,bd=3,relief="ridge")
-label.pack(side="top",fill="x", expand=True)
+label.pack(side="top",fill="both", expand=True)
 
 
 #frame
 frame = Frame(window)
-frame.pack()
+frame.pack(fill="both" ,expand=True)
 
-  
-  
+for i in range(4):
+    frame.grid_columnconfigure(i, weight=1)
+for j in range(5):
+    frame.grid_rowconfigure(j, weight=1)
 
 #buttons
 
 button1 = Button(frame,text="1",width=4,height=3,command= lambda:button_pressing(1))
-button1.grid(column=0,row=0)
+button1.grid(column=0,row=0, sticky="nsew")
 
   
 button2 = Button(frame,text="2",width=4,height=3,command= lambda:button_pressing(2))
-button2.grid(column=1,row=0)
+button2.grid(column=1,row=0, sticky="nsew")
 
   
 button3 = Button(frame,text="3",width=4,height=3,command= lambda:button_pressing(3))
-button3.grid(column=2,row=0)
+button3.grid(column=2,row=0, sticky="nsew")
 
   
 button4 = Button(frame,text="4",width=4,height=3,command= lambda:button_pressing(4))
-button4.grid(column=0,row=1)
+button4.grid(column=0,row=1, sticky="nsew")
 
   
 button5 = Button(frame,text="5",width=4,height=3,command= lambda:button_pressing(5))
-button5.grid(column=1,row=1)
+button5.grid(column=1,row=1, sticky="nsew")
 
   
 button6 = Button(frame,text="6",width=4,height=3,command= lambda:button_pressing(6))
-button6.grid(column=2,row=1)
+button6.grid(column=2,row=1, sticky="nsew")
 
 
 button7 = Button(frame,text="7",width=4,height=3,command= lambda:button_pressing(7))
-button7.grid(column=0,row=2)
+button7.grid(column=0,row=2, sticky="nsew")
 
   
 button8 = Button(frame,text="8",width=4,height=3,command= lambda:button_pressing(8))
-button8.grid(column=1,row=2)
+button8.grid(column=1,row=2, sticky="nsew")
 
   
 button9 = Button(frame,text="9",width=4,height=3,command= lambda:button_pressing(9))
-button9.grid(column=2,row=2)
+button9.grid(column=2,row=2, sticky="nsew")
 
 
 
 
 button0 = Button(frame, text=0, height=4, width=3,command=lambda: button_pressing(0))
-button0.grid(row=3, column=0)
+button0.grid(row=3, column=0, sticky="nsew")
 
 
 plus = Button(frame, text='+', height=4, width=3,command=lambda: button_pressing('+'))
-plus.grid(row=0, column=3)
+plus.grid(row=0, column=3, sticky="nsew")
 
 
 minus = Button(frame, text='-', height=4, width=3,command=lambda: button_pressing('-'))
-minus.grid(row=1, column=3)
+minus.grid(row=1, column=3, sticky="nsew")
 
   
 multiply = Button(frame, text='*', height=4, width=3,command=lambda: button_pressing('*'))
-multiply.grid(row=2, column=3)
+multiply.grid(row=2, column=3, sticky="nsew")
 
   
 divide = Button(frame, text='/', height=4, width=3,command=lambda: button_pressing('/'))
-divide.grid(row=3, column=3)
+divide.grid(row=3, column=3, sticky="nsew")
 
 
 equal = Button(frame, text='=', height=4, width=3,command=equals)
-equal.grid(row=3, column=2)
+equal.grid(row=3, column=2, sticky="nsew")
 
 
 decimal = Button(frame, text='.', height=4, width=3,command=lambda: button_pressing('.'))
-decimal.grid(row=3, column=1)
+decimal.grid(row=3, column=1, sticky="nsew")
 
   
 
 clear_screan = Button(frame, text='clear', height=4, width=12,command=clear)
-clear_screan.grid(row=4, column=0)
+clear_screan.grid(row=4, column=0, columnspan=4, sticky="nsew")
 
 window.mainloop()
